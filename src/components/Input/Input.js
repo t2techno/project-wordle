9da11e6f2cf5c7b02b116685c55f991e1f356e65
t2techno <ttuschhoff11@gmail.com>
2024-03-12 23:05:06 -0500

    removing c&p issues

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,13 +1,5 @@
 import React from "react";
 
-import { sample } from "../../utils";
-import { WORDS } from "../../data";
-
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
-
 function Input({ handleSubmit }) {
   const [value, setValue] = React.useState("");
   const onSubmit = (event) => {
